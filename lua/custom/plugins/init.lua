f6a652d243568a158dcc8efea67129b5ddf6c63e
Sylvain Maissan <sylvain.maissan@essent.nl>
2025-05-21 15:21:11 +0200

    Add changes to init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,28 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      'rcarriga/nvim-notify',
+    },
+  },
+  {
+    'vimpostor/vim-tpipeline',
+    config = function()
+      vim.g.tpipeline_autoembed = 1
+      vim.g.tpipeline_restore = 1
+      vim.g.tpipeline_clearstl = 1
+    end,
+  },
+}
